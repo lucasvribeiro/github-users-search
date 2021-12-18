374f88811bf697ebe7c4face1ef18a2c0fa9ce67
lucasvribeiro <lucasvribeiro14@gmail.com>
2021-12-18 15:46:06 -0300

    Created SearchBox Component

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,7 +1,109 @@
-import React from "react";
+import styled from "styled-components";
 
-const SearchBox = () => {
-  return <div>search box</div>;
+import loading from "../../images/loading.gif";
+import lens from "../../images/lens.png";
+
+const SearchStyled = styled.div`
+  --background: #ffffff;
+  --text-color: #414856;
+  --primary-color: #111111;
+  --width: 90vw;
+  --height: 64px;
+
+  background: var(--background);
+  width: var(---width);
+  height: var(--height);
+  max-width: 600px;
+  overflow: hidden;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  box-shadow: 1px 4px 22px -7px rgba(0, 0, 0, 0.3);
+
+  position: absolute;
+  transition: all 0.6s ease;
+  top: 50%;
+
+  /* show button when input has any text */
+  input:not(:placeholder-shown) {
+    ~ .submit-button {
+      display: block !important;
+      background: #111111 !important;
+      width: 60px !important;
+    }
+  }
+
+  input[type="text"] {
+    position: relative;
+    width: var(--width);
+    height: var(--height);
+    font-size: 1rem;
+    color: var(--text-color);
+    border: 0;
+    box-sizing: border-box;
+    outline: none;
+    padding: 0 0 0 50px;
+  }
+
+  .lens {
+    width: 20px;
+    top: 20px;
+    left: 20px;
+
+    position: absolute;
+    filter: invert(0%) sepia(3%) saturate(6%) hue-rotate(326deg) brightness(95%)
+      contrast(87%);
+  }
+
+  .submit-button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 0;
+    font-weight: bold;
+    background: #ffffff;
+    border-radius: 0 10px 10px 0;
+
+    height: 100%;
+    border: 0;
+    color: #ffffff;
+    font-size: 1rem;
+
+    transition: all 0.3s cubic-bezier(0, 0, 0.43, 1.49);
+    cursor: pointer;
+  }
+
+  .loading {
+    width: 24px;
+  }
+`;
+
+// const StyledButton = styled.button``;
+
+const SearchBox = (props) => {
+  return (
+    <SearchStyled style={props.user ? { top: "10%" } : { top: "50%" }}>
+      <input
+        type="text"
+        placeholder="buscar usuário..."
+        disabled={props.loading}
+        onChange={props.onChange}
+      />
+      <div className="symbol">
+        <img src={lens} alt="Search Icon" className="lens" />
+      </div>
+
+      <button type="submit" className="submit-button" onClick={props.onClick}>
+        {props.loading ? (
+          <img src={loading} alt="Loading" className="loading" />
+        ) : (
+          "Ir"
+        )}
+      </button>
+    </SearchStyled>
+  );
 };
 
 export default SearchBox;
--- src/pages/SearchPage/SearchPage.jsx
+++ src/pages/SearchPage/SearchPage.jsx
@@ -1,25 +1,50 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import SearchBox from "../../components/SearchBox/SearchBox";
+
+import "./SearchPage.css";
 
 const SearchPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState();
+  const [user, setUser] = useState();
   const token = process.env.REACT_APP_GITHUB_TOKEN;
 
-  useEffect(() => {
+  const goSearch = () => {
+    setLoading(true);
+
     axios
-      .get(`https://api.github.com/users/luc`, {
+      .get(`https://api.github.com/users/${searchValue}`, {
         headers: {
           Authorization: `${token}`,
         },
       })
       .then((res) => {
         console.log(res.data);
+        setUser(res.data);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err.response);
+        setLoading(false);
       });
-  }, [token]);
+  };
 
-  return <div>SearchPage</div>;
+  const searchChanged = (e) => {
+    console.log(e.target.value);
+    setSearchValue(e.target.value);
+  };
+
+  return (
+    <div className="search-page">
+      <SearchBox
+        onChange={searchChanged}
+        onClick={goSearch}
+        loading={loading}
+        user={user}
+      />
+    </div>
+  );
 };
 
 export default SearchPage;
